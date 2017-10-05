8b2c89a8aca3d33f97af3a5e7f26d4f07503db0e
Shaun Ibanez <stibanez@student.apc.edu.ph>
2017-10-05 19:19:49 +0800

    Added models/Room.php

--- Web Services/basic/config/web.php
+++ Web Services/basic/config/web.php
@@ -10,7 +10,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'ariana',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
