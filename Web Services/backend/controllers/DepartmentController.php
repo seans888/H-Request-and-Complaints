8b1418b51a13a996d8df7c15627b46c12a29fd16
Shaun Ibanez <stibanez@student.apc.edu.ph>
2017-10-11 18:06:25 +0800

    Added backend yii2

---
+++ Web Services/backend/controllers/DepartmentController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class DepartmentController extends ActiveController
+{
+    public $modelClass = 'app\models\Department';
+}
