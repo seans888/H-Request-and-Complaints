b8b359c00eb09168d299118b6cba95bdb1480132
zgvertudes <ziandenisse@gmail.com>
2017-10-24 22:59:35 +0800

    Dashboards

---
+++ Web Services/RTMSOfficial1/build/generated/src/org/apache/jsp/LogIn_jsp.java
@@ -0,0 +1,87 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class LogIn_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("    </head>\n");
+      out.write("\n");
+      out.write("    <link rel=\"stylesheet\" href=\"newcss.css\">\n");
+      out.write("    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Raleway\">\n");
+      out.write("    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n");
+      out.write("\n");
+      out.write("    <body class=\"w3-blue\">\n");
+      out.write("        <div class =\"w3-card w3-white w3-display-middle w3-quarter\">\n");
+      out.write("            <h1 class=\"w3-center\">Login</h1>\n");
+      out.write("            <form class=\"w3-container\" action=\"Validate\" method=\"POST\"></br>\n");
+      out.write("\n");
+      out.write("                <label>Username</label>\n");
+      out.write("                <input class=\"w3-input\" type=\"text\" name=\"username\"></br>\n");
+      out.write("\n");
+      out.write("                <label>Password</label>\n");
+      out.write("                <input class=\"w3-input\" type=\"password\" name=\"password\"></br>\n");
+      out.write("               \n");
+      out.write("                <button class=\"w3-btn w3-blue w3-block\" type=\"submit\">Login</button></br>\n");
+      out.write("                \n");
+      out.write("            </form>\n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
